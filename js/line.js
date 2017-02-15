2a75cdba625ed9b74edc8b5a1090ac6337b6197d
李梦龙 <649076408@qq.com>
2017-02-16 00:13:14 +0800

    edit

--- js/line.js
+++ js/line.js
@@ -3,6 +3,8 @@ function Water() {
   this.initVar();
   //设置 canvas 的宽高
   this.setCanvas();
+  this.oCtx.fillStyle = 'black';
+  this.oCtx.fillRect( 0, 0, this.iWinWidth, this.iWinHeight );
   //生成一个点
   // this.createPoint();
   //动画
@@ -18,6 +20,12 @@ Water.prototype.initVar = function () {
   this.oAnimationFrame = null; //帧数变化的名字
   this.iPointX = this.iWinWidth / 2; //点的起始位置 x
   this.iPointY = this.iWinHeight / 2; //点的起始位置 y
+  this.tick = 0;
+  this.cumulativeTime = 0;
+  this.time = 0;
+
+  this.v = 6;
+  this.repaintAlpha = .04;
 };
 //设置 canvas 的宽高
 Water.prototype.setCanvas = function () {
@@ -27,9 +35,31 @@ Water.prototype.setCanvas = function () {
 //生成一个点
 Water.prototype.createPoint = function () {
 
-  this.iPointX -= 6;
-  this.iPointY -= 6;
-  this.oCtx.fillRect( this.iPointX, this.iPointY, 5, 5 );
+    ++this.time;
+    ++this.cumulativeTime;
+
+  if (this.iPointY < 0) {
+    // this.iPointY += this.v;
+    // canelAnimationFrame(this.oAnimationFrame)
+  }else{
+    this.iPointY += -this.v;
+  }
+  if (this.iPointY < 0) {
+    this.iPointX += this.v;
+  }else{
+    this.iPointX += -this.v;
+  }
+
+  this.color = 'hsl(hue,100%,light%)'.replace( 'hue', 100 );
+  this.lightInputMultiplier = 0.01 + 0.02 * Math.random();
+
+  // this.oCtx.fillStyle = this.color.replace( 'light',50 + 10 * Math.sin( this.cumulativeTime * this.lightInputMultiplier ) );
+  // this.oCtx.fillRect( this.iPointX, this.iPointY, 5, 5 );
+
+  this.oCtx.fillStyle = this.oCtx.shadowColor = this.color.replace( 'light', 50 + 10 * Math.sin( this.cumulativeTime * this.lightInputMultiplier ) );
+
+  this.oCtx.fillRect( this.iPointX, this.iPointY, 5,5 );
+
 
 };
 //动画
@@ -38,10 +68,12 @@ Water.prototype.drawframe = function (){
     this.oAnimationFrame = requestAnimationFrame(this.drawframe.bind(this));
     this.oCtx.globalCompositeOperation = 'source-over';
     this.oCtx.shadowBlur = 0;
-    this.oCtx.fillStyle = 'rgba(255,0,0,alp)'.replace( 'alp',0.4 );
-    this.oCtx.fillRect( 0, 0, 5, 5 );
+    this.oCtx.fillStyle = 'rgba(0,0,0,alp)'.replace( 'alp', this.repaintAlpha );
+    this.oCtx.fillRect( 0, 0, this.iWinWidth, this.iWinHeight );
     this.oCtx.globalCompositeOperation = 'lighter';
 
+    //生成一个点
+    this.createPoint();
 };
 
 //程序的初始化
