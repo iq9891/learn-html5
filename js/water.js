f43c59267dfbd181d1e53e12ac6f8c705f9f5fe0
leerb <914726892@qq.com>
2017-02-15 16:18:52 +0800

    end water

--- js/water.js
+++ js/water.js
@@ -9,92 +9,105 @@ function Water() {
   this.drawWater();
   //动画
 	this.drawframe();
+
 }
 //声明参数
 Water.prototype.initVar = function () {
   this.oCanvas = document.getElementById('canvas');
   this.oCtx = this.oCanvas.getContext('2d');
-	this.iWinWidth = window.innerWidth;
-	this.iWinHeight = window.innerHeight;
-  this.color1 = "#6ca0f6"; //海水颜色
-  this.vertexes = [];    //顶点坐标
-  this.verNum = 250;     //顶点数
-  this.diffPt = [];      //差分值
-  this.autoDiff = 1000;  //初始差分值
-	this.vPos = 125;  //震荡点
-	this.iBuffer = 15;     //缓冲
-	this.friction = 0.15; //摩擦力
-	this.deceleration = 0.95; //减速
+  this.iWinWidth = window.innerWidth;
+  this.iWinHeight = window.innerHeight;
+  this.sColor = "#6ca0f6"; //海水颜色
+  this.aVertexes = []; //顶点坐标
+  this.iVerNum = 1000; //顶点数
+  this.iInitialDiff = 1000; //初始差分值
+  this.iStartingPoint = this.iVerNum / 2; //震荡点
+  this.iBuffer = 15;//38; //缓冲 数字太小中间会有不流畅的过渡
+  this.iFriction = 0.15; //摩擦力
+  this.iDeceleration = 0.95; //减速
   this.oAnimationFrame = null;
 };
 //设置 canvas 的宽高
 Water.prototype.setCanvas = function () {
-	this.oCanvas.width = this.iWinWidth;
-	this.oCanvas.height = this.iWinHeight;
+  this.oCanvas.width = this.iWinWidth;
+  this.oCanvas.height = this.iWinHeight;
 };
 //生成顶点，初始差分值
 Water.prototype.initVertex = function () {
-  for(var i=0; i< this.verNum; i++){
-		this.vertexes[i] = {
-    	x: this.iWinWidth / (this.verNum-1) * i, // 点的坐标
+  for(var i=0; i< this.iVerNum; i++){
+		this.aVertexes[i] = {
+    	x: this.iWinWidth / (this.iVerNum-1) * i, // 点的坐标
     	y: this.iWinHeight/2,
     	vy: 0, //竖直方向的速度
-      diffPt: 0
+      //如果是起震点，就初始差分值
+      diffPt: i === this.iStartingPoint ? this.iInitialDiff : 0  //初始差分值
     };
-		this.diffPt[i] = 0;
 	}
 };
 //设置 canvas 的水
 Water.prototype.drawWater = function () {
-  this.oCtx.save();
-  this.oCtx.fillStyle = this.color1;
+  //清空子路径列表开始一个新路径的方法。
   this.oCtx.beginPath();
-  this.oCtx.moveTo(0, this.iWinHeight);
-  this.oCtx.lineTo(0, this.iWinHeight / 2);
-  for(var i=1; i<this.vertexes.length; i++){
-  	this.oCtx.lineTo(this.vertexes[i].x, this.vertexes[i].y);
-  }
+  //画水
+  this.aVertexes.map(function (oVertex, oVertexId) {
+    this.oCtx.lineTo(oVertex.x, oVertex.y);
+  }.bind(this));
+  //从顶点的最后一个点，向( this.iWinWidth, this.iWinHeight )点画线
   this.oCtx.lineTo(this.iWinWidth, this.iWinHeight);
+  //从( this.iWinWidth, this.iWinHeight )点，向( 0, this.iWinHeight )点画线
   this.oCtx.lineTo(0, this.iWinHeight);
+  //设置颜色
+  this.oCtx.fillStyle = this.sColor;
+  //填充
   this.oCtx.fill();
-  this.oCtx.restore();
+
+};
+//顶点恢复
+Water.prototype.reset = function(){
+
+  this.aVertexes.map(function (oVertex, oVertexId) {
+    oVertex.y = this.iWinHeight/2;
+    oVertex.vy = 0; //竖直方向的速度
+    //如果是起震点，就初始差分值
+    oVertex.diffPt = oVertexId === this.iStartingPoint ? this.iInitialDiff : 0;  //初始差分值
+  }.bind(this));
+
 };
 //顶点更新
 Water.prototype.update = function(){
-  this.autoDiff -= this.autoDiff*0.9;
-  this.diffPt[this.vPos] = this.autoDiff;
-  //更新左右
-  for (var h = 0; h < this.vPos; h++) {
-    console.log((this.diffPt[this.vPos - h - 1] - this.diffPt[this.vPos - h]) * (1 - 0.01 * h), (this.diffPt[this.vPos + h + 1] - this.diffPt[this.vPos + h]) * (1 - 0.01 * h));
-    //更新零界点左边的
-    this.diffPt[this.vPos - h - 1] -= (this.diffPt[this.vPos - h - 1] - this.diffPt[this.vPos - h]) * (1 - 0.01 * h);
-    //更新零界点右边的
-    this.diffPt[this.vPos + h + 1] -= (this.diffPt[this.vPos + h + 1] - this.diffPt[this.vPos + h]) * (1 - 0.01 * h);
-  }
+
+  this.aVertexes[this.iStartingPoint].diffPt -= this.aVertexes[this.iStartingPoint].diffPt * 0.9;
   //更新Y坐标
-  for(var i=0; i < this.vertexes.length; i++){
-    this.vertexes[i].diffPt -= (this.vertexes[i].diffPt - this.vertexes[i + 1 * ( (i - this.vPos) < 0 ? 1 : -1 )].diffPt) * (1 - 0.01 * i);
-    this.vertexes[i].vy += (this.diffPt[i] + this.iWinHeight / 2 - this.vertexes[i].y);
-  	this.vertexes[i].vy *= this.deceleration;
-  	this.vertexes[i].y += this.vertexes[i].vy * this.friction;
-    console.log(this.vertexes[i].diffPt);
-  }
-  //如果速度为0了就停止动画
-  if (Math.abs(this.vertexes[this.vPos].vy).toFixed(3) < '0.001') {
-    cancelAnimationFrame(this.oAnimationFrame);
-  }
+  this.aVertexes.map(function (oVertex, oVertexId) {
+    if (oVertexId < this.iStartingPoint) {
+      this.aVertexes[this.iStartingPoint - oVertexId - 1].diffPt -= ( this.aVertexes[this.iStartingPoint - oVertexId - 1].diffPt - this.aVertexes[this.iStartingPoint - oVertexId].diffPt ) * (1 - 0.01 * (oVertexId > this.iBuffer ? this.iBuffer : oVertexId ));
+      if (this.iStartingPoint + oVertexId + 1 < this.aVertexes.length) {
+        //右边的 = 左边的
+        this.aVertexes[this.iStartingPoint + oVertexId + 1].diffPt = this.aVertexes[this.iStartingPoint - oVertexId - 1].diffPt;
+      }
+    }
+    oVertex.vy += (oVertex.diffPt + this.iWinHeight / 2 - oVertex.y);
+  	oVertex.vy *= this.iDeceleration;
+  	oVertex.y += oVertex.vy * this.iFriction;
+  }.bind(this));
 
 };
 //动画
 Water.prototype.drawframe =function (){
-  var self = this;
   //更新坐标点
   this.oCtx.clearRect(0, 0, this.iWinWidth, this.iWinHeight);
-  // this.oAnimationFrame = requestAnimationFrame(function () {
-  //   self.drawframe();
-  // }, this.oCanvas);
   this.update();
   this.drawWater();
+  //添加循环帧数
+  this.oAnimationFrame = requestAnimationFrame(this.drawframe.bind(this));
+  //如果速度为0了就停止动画
+  if (Math.abs(this.aVertexes[this.iStartingPoint].vy).toFixed(3) < 0.01) { //75.529 //0.01
+    //取消
+    cancelAnimationFrame(this.oAnimationFrame);
+    this.oAnimationFrame = null;
+    this.reset();
+    this.drawWater();
+  }
 };
 
 //所有程序的初始化
